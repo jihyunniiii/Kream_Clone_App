3531f116307fd4a3ec22cee79f54ff4c3971b297
jihyunniiii <jita1125@naver.com>
2023-02-20 16:35:17 +0900

    Brand Fragment 추가

---
+++ app/src/main/java/com/jihyun/kreamclone/BrandFragment.kt
@@ -0,0 +1,24 @@
+package com.jihyun.kreamclone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BrandFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_brand, container, false)
+    }
+}
