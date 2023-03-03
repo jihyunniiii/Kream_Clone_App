9321830cff1be358299dbbe6549ec8670d1dfad0
jihyunniiii <jita1125@naver.com>
2023-03-03 20:23:55 +0900

    release info binding 사전 작업

--- app/src/main/java/com/jihyun/kreamclone/ReleaseInfoFragment.kt
+++ app/src/main/java/com/jihyun/kreamclone/ReleaseInfoFragment.kt
@@ -5,19 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jihyun.kreamclone.databinding.FragmentReleaseInfoBinding
 
 class ReleaseInfoFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    private var _binding: FragmentReleaseInfoBinding? = null
+    private val binding: FragmentReleaseInfoBinding
+        get() =  requireNotNull(_binding)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_release_info, container, false)
+        _binding = FragmentReleaseInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
